81796aecbc43ca17b5599b941eb4abc3d60b5825
Hanu Shashwat <hanushashwat733@gmail.com>
2026-03-09 00:56:02 +0530

    Built the BLoC logic for App Bar buttons.

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -6,8 +8,39 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<HomeProductWishlistButtonClickedEvent>(
+      homeProductWishlistButtonClickedEvent,
+    );
+    on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
+    on<HomeWishlistButtonNavigateEvent>(homeWishlistButtonNavigateEvent);
+    on<HomeCartButtonNavigateEvent>(homeCartButtonNavigateEvent);
+  }
+
+  FutureOr<void> homeProductWishlistButtonClickedEvent(
+    HomeProductWishlistButtonClickedEvent event,
+    Emitter<HomeState> emit,
+  ) {
+    print('Wishlist clicked.');
+  }
+
+  FutureOr<void> homeProductCartButtonClickedEvent(
+    HomeProductCartButtonClickedEvent event,
+    Emitter<HomeState> emit,
+  ) {
+    print('Cart clicked.');
+  }
+
+  FutureOr<void> homeWishlistButtonNavigateEvent(
+    HomeWishlistButtonNavigateEvent event,
+    Emitter<HomeState> emit,
+  ) {
+    print('Wishlist Page Route Button Clicked.');
+  }
+
+  FutureOr<void> homeCartButtonNavigateEvent(
+    HomeCartButtonNavigateEvent event,
+    Emitter<HomeState> emit,
+  ) {
+    print('Cart Page Route Button Clicked.');
   }
 }
--- lib/features/home/ui/home_page.dart
+++ lib/features/home/ui/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:grocery_ecomm/features/home/bloc/home_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final HomeBloc homeBloc = HomeBloc();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hanu's Grocery App"),
-      ),
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,
+      // listenWhen: (previous, current) => {},
+      // buildWhen: (previous, current) => {},
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Hanu's Grocery App"),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  homeBloc.add(HomeWishlistButtonNavigateEvent());
+                },
+                icon: Icon(Icons.favorite_border),
+              ),
+              IconButton(
+                onPressed: () {
+                  homeBloc.add(HomeCartButtonNavigateEvent());
+                },
+                icon: Icon(Icons.shopping_bag_outlined),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
